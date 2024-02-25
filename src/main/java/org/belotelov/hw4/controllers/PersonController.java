9d5946d6b41cd8505f5f7cd2727bfca7db05bec7
Maksim <maksim_belotelov@mail.ru>
2024-02-25 22:26:46 +0300

    added homework 08

--- src/main/java/org/belotelov/hw4/controllers/PersonController.java
+++ src/main/java/org/belotelov/hw4/controllers/PersonController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@Log
 @AllArgsConstructor
 @RequestMapping("/")
 public class PersonController {
@@ -23,14 +22,12 @@ public class PersonController {
     public String showAllPersons(Model model) {
         List<Person> persons = personService.findAllPersons();
         model.addAttribute("persons", persons);
-        log.info("Received reqiest to show all persons");
         return "index";
     }
 
     @GetMapping("/new")
     public String showNewPersonForm(Model model) {
         model.addAttribute("person", new Person());
-        log.info("Received request to show form for adding new person");
         return "new";
     }
 
@@ -38,12 +35,10 @@ public class PersonController {
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            log.info("Received incorrect data in form");
             return "new";
         }
 
         personService.addPerson(person);
-        log.info("Added new person " + person.getName());
         return "redirect:/";
     }
 
@@ -51,25 +46,21 @@ public class PersonController {
     public String updateUserForm(@PathVariable("id") int id, Model model) {
         Person person = personService.getOne(id);
         model.addAttribute("person", person);
-        log.info("Received request to show form for editing person");
         return "update";
     }
 
     @PostMapping("/update")
     public String updatePerson(@Valid Person person, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            log.info("Received incorrect data in form");
             return "update";
         }
         personService.updatePerson(person);
-        log.info("Updated person " + person.getName());
         return "redirect:/";
     }
 
     @GetMapping("/delete/{id}")
     public String deletePerson(@PathVariable("id") int id) {
         personService.deletePersonById(id);
-        log.info("Deleted person with id=" + id);
         return "redirect:/";
     }
 }
--- src/main/java/org/belotelov/hw4/service/PersonService.java
+++ src/main/java/org/belotelov/hw4/service/PersonService.java
@@ -1,6 +1,7 @@
 package org.belotelov.hw4.service;
 
 import lombok.AllArgsConstructor;
+import org.belotelov.hw4.aspects.TrackUserAction;
 import org.belotelov.hw4.model.Person;
 import org.belotelov.hw4.repository.PersonRepository;
 import org.springframework.stereotype.Service;
@@ -11,20 +12,18 @@ import java.util.List;
 public class PersonService {
     private final PersonRepository personRepository;
 
+    @TrackUserAction
     public List<Person> findAllPersons() {
         return personRepository.findAll();
     }
-
+    @TrackUserAction
     public Person getOne(int id) { return personRepository.getOne(id); }
+    @TrackUserAction
     public void addPerson(Person person) {
         personRepository.save(person);
     }
-
-    public Person savePerson(Person person) {
-        return personRepository.save(person);
-    }
-
+    @TrackUserAction
     public void deletePersonById(int id) { personRepository.deleteById(id); }
-
+    @TrackUserAction
     public Person updatePerson(Person person) { return personRepository.update(person); }
 }
